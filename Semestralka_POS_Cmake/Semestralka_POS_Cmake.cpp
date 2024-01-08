a9f105d8e238fc04c2a9c59ab893e248457a8a4f
jakub-2 <jakubjanco@gmail.com>
2024-01-08 20:15:38 +0100

    Add project files.

---
+++ Semestralka_POS_Cmake/Semestralka_POS_Cmake.cpp
@@ -0,0 +1,12 @@
+﻿// Semestralka_POS_Cmake.cpp : Defines the entry point for the application.
+//
+
+#include "Semestralka_POS_Cmake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
